6da820dddaf2aab9b840666d1a3a214c2cce87ac
jack <75663899+lianchaoqi@users.noreply.github.com>
2022-12-28 16:15:22 +0800

    Update GitTest.java
    pull test

--- Mybatis-demo3/src/main/java/com/jack/mybatis/test/GitTest.java
+++ Mybatis-demo3/src/main/java/com/jack/mybatis/test/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("master aaaa");
         System.out.println("hot fix test");
+         System.out.println("pull pull pull");
 
     }
 }
