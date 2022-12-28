794b02ffc7cdf4841197ec07079d371eae9152f4
jack <2504810905@qq.com>
2022-12-28 15:15:58 +0800

    master1提交

---
+++ Mybatis-demo3/src/main/java/com/jack/mybatis/test/GitTest.java
@@ -0,0 +1,19 @@
+package com.jack.mybatis.test;
+
+/**
+ * @BelongsProject: Mybatis
+ * @BelongsPackage: com.jack.mybatis.test
+ * @Author: jack
+ * @CreateTime: 2022-12-28  14:57
+ * @Description: TODO
+ * @Version: jdk1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(11);
+        System.out.println(22);
+        System.out.println(33);
+        System.out.println("master test");
+
+    }
+}
